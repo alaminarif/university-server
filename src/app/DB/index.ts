b5aa429be8a43d117cf0e2cb3a3884a127483fa2
Arifur Rahman <arifur3429@gmail.com>
2024-08-23 15:44:43 +0600

    add multer done

--- src/app/DB/index.ts
+++ src/app/DB/index.ts
@@ -3,7 +3,7 @@ import { USER_ROLE } from '../modules/User/user.constant';
 import { User } from '../modules/User/user.model';
 
 const superUser = {
-  // id: '0001',
+  id: '0001',
   email: 'arifurr231@gmail.com',
   password: config.super_admin_password,
   needsPasswordChange: false,
